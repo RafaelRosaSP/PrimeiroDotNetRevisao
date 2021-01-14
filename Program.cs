31db56c83fad1d28dc47b01c5df47eb503eebc1c
RafaelRosaSPgit config --global user.name RafaelRosaSPgit config --global user.username RafaelRosaSP <rafael@pilotomaster.com.br>
2021-01-14 10:26:36 -0300

    Iniciando Revisao C# .Net

--- Program.cs
+++ Program.cs
@@ -6,7 +6,11 @@ namespace PrimeiroDotNetRev
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine("1- Inserir novo aluno");
+            Console.WriteLine("2- Listar alunos");
+            Console.WriteLine("Calcular média geral");
+            Console.WriteLine();
         }
     }
 }
